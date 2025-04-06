162ebcfe5de5732a237e4a5cc7628cc7dcdde7c0
emheyboer <157163774+emheyboer@users.noreply.github.com>
2025-04-06 19:34:59 -0400

    Initial Commit

---
+++ isTheLibraryOpen/isTheLibraryOpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  isTheLibraryOpenApp.swift
+//  isTheLibraryOpen
+//
+//  Created by emh on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct isTheLibraryOpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
